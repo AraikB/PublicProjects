35fe472a69683bb6a953395d41313ca5c1297b8b
Araik Barouni <ABarouni@icsecurity.com>
2022-07-19 14:31:17 -0700

    Comments and formatting

--- tip_calculator_day_2/tip_calculator.py
+++ tip_calculator_day_2/tip_calculator.py
@@ -32,43 +32,45 @@ def simple_version():
     print(f"Each person should pay: ${split_amount:.2f}")
 
 
-def greeting() -> None:
-    print("Welcome to the tip calculator!")
+class TipCalculator:
+    """## TipCalculator
 
+    Takes the total Bill amount, number of people, and the tip percentage desired to be left and returns the price due per person
+    """
 
-def get_total_bill() -> float:
-    total_bill_amount = float(input("What was the total bill? "))
-    return total_bill_amount
+    def __init__(self) -> None:
+        self.greeting()
+        self.total_bill_amount = self.get_total_bill()
+        self.number_of_people = self.get_number_of_people()
+        self.tip_percentage = self.get_tip_percentage()
 
+    def greeting(self) -> None:
+        print("Welcome to the tip calculator!")
 
-def get_tip_percentage() -> float:
-    tip_percentage = input("How much tip would you like to give? 10%, 12%, 15%")
-    tip_percentage = float(tip_percentage.replace("%", ""))
-    return tip_percentage
+    def get_total_bill(self) -> float:
+        total_bill_amount = float(input("What was the total bill? "))
+        return total_bill_amount
 
+    def get_tip_percentage(self) -> float:
+        tip_percentage = input("How much tip would you like to give? 10%, 12%, 15% ")
+        tip_percentage = float(tip_percentage.replace("%", ""))
+        return tip_percentage
 
-def get_number_of_people() -> float:
-    number_of_people = float(input("How many people will be splitting the bill?"))
-    return number_of_people
+    def get_number_of_people(self) -> float:
+        number_of_people = float(input("How many people will be splitting the bill? "))
+        return number_of_people
 
-
-def calculate_tip(
-    total_bill_amount: float, number_of_people: float, tip_percentage: float
-) -> str:
-    total_with_tip = total_bill_amount * (1 + tip_percentage / 100)
-    split_amount = total_with_tip / number_of_people
-    return f"Each person should pay: ${split_amount:.2f}"
-
-
-def main():
-    bill = get_total_bill()
-    tip = get_tip_percentage()
-    people = get_number_of_people()
-    tip = calculate_tip(bill, people, tip)
-
-    print(tip)
+    def calculate_amount_per_person(
+        self,
+    ) -> str:
+        self.tip_percentage: float
+        total_with_tip = self.total_bill_amount * (1 + self.tip_percentage / 100)
+        split_amount = total_with_tip / self.number_of_people
+        return f"Each person should pay: ${split_amount:.2f}"
 
 
 if __name__ == "__main__":
     #  main()
-    simple_version()
+    # simple_version()
+    my_calc = TipCalculator()
+    print(my_calc.calculate_amount_per_person())
